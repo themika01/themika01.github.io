9b7c015f52b2f2bd9a47e21926234b0fdb907b0c
Miiiiika <itunesayylmao3@gmail.com>
2023-05-25 18:22:13 -0700

    Basic Buttons on Layout
    Rearranged website layout. Made buttons for "about this site", "social media", etc. Added JS so that the buttons will show a div containing the relevant content. ATM only the "About This Site" div actually has content.

---
+++ script.js
@@ -0,0 +1,39 @@
+console.log("Howdy partner") //Confirm that JS is running
+
+//Defining page elements
+const btnAbout = document.getElementById("about")
+const btnSocials = document.getElementById("socials")
+const btnMusic = document.getElementById("music")
+const btnMovie = document.getElementById("movie")
+const aboutBtns = [btnAbout, btnSocials, btnMusic, btnMovie]
+
+const conAbout = document.getElementById("about-content")
+const conSocial = document.getElementById("social-content")
+const conMusic = document.getElementById("music-content")
+const conMovie = document.getElementById("movie-content")
+const aboutContent = [conAbout, conSocial, conMusic, conMovie]
+
+//Showing divs containing content from the about me sections
+let lastSelect = -1
+shown = false
+const showAboutContent = function(btnNumber) {
+    for (let i = 0; i<aboutContent.length; i++) { //Close other windows
+        aboutContent[i].className = "button-content-hidden"
+        aboutBtns[i].className = "more-information"
+    }
+    if (shown == false || shown == true && lastSelect != btnNumber) {
+        aboutBtns[btnNumber].className = "more-information-selected"
+        aboutContent[btnNumber].className = "button-content-shown"
+        lastSelect = btnNumber
+        shown = true
+    } else { //Hide all content if the user clicks on the same button they opened
+        lastSelect = -1
+        shown = false
+    }
+}
+
+//Event listeners
+btnAbout.addEventListener("click", function(){showAboutContent(0)})
+btnSocials.addEventListener("click", function(){showAboutContent(1)})
+btnMusic.addEventListener("click", function(){showAboutContent(2)})
+btnMovie.addEventListener("click", function(){showAboutContent(3)})
