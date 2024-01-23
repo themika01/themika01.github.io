dcbf9b9b125fd476dab52fd8477159b337f14297
Miiiiika <jjpound@protonmail.com>
2024-01-23 00:37:54 -0700

    Dropdown Menu Addition
    Added dropdown menu links for nav bar to different articles. Actual link navigation will be added later once I start writing articles.

---
+++ script.js
@@ -0,0 +1,23 @@
+// Defining dropdown sections
+let generalSection = document.getElementById("general-history-section")
+let fascismsSection = document.getElementById("fascisms-section");
+let figuresSection = document.getElementById("figures-section");
+let furtherSection = document.getElementById("further-section");
+// Sections array
+let sections = [generalSection, fascismsSection, figuresSection, furtherSection];
+
+function openDrop(number) {
+    sections[number].classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropBtn')) {
+      var i;
+      for (i = 0; i < sections.length; i++) {
+        var openDropdown = sections[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  } 
