8f9ccaf5428e9be4acb9e323e1f411f6aaabd675
Miiiiika <jjpound@protonmail.com>
2024-01-30 18:37:55 -0700

    Minor style changes
    Based on suggestions from Lav

--- script.js
+++ script.js
@@ -7,18 +7,20 @@ let mobileSection = document.getElementById("mobile-section");
 // Sections array
 let sections = [generalSection, fascismsSection, figuresSection, furtherSection, mobileSection];
 
+// Open/close the section clicked on, section provided by button as number variable
 function openDrop(number) {
     sections[number].classList.toggle("show");
 }
 
+// Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
-    if (!event.target.matches('.dropBtn')) {
-      var i;
-      for (i = 0; i < sections.length; i++) {
-        var openDropdown = sections[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+  if (!event.target.matches('.dropBtn')) {
+    var i;
+    for (i = 0; i < sections.length; i++) {
+      var openDropdown = sections[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
     }
-  } 
+  }
+} 
